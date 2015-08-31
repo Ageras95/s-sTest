8bbd5536db34b31149ebaa7fe378edebe26eef70
ebouma <ebouma@wsrob.s-s-systems.nl>
2015-08-31 14:32:52 +0200

    150

--- src/project1.java
+++ src/project1.java
@@ -13,7 +13,7 @@ public class project1 {
 	        try
 	        {
 	        	int test = 0;
-	        	int test2 = 123;
+	        	int test2 = 150;
 	            int number = Integer.parseInt(reader.readLine());
 	            System.out.println("You entered: " + number);
 	            System.out.println("Test getal is:  " + test);
