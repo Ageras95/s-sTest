a3c17c4e3b779cc08abb2e668ebb5a09415dffcf
Ageras95 <j.hoekstra31@gmail.com>
2015-08-31 13:32:53 +0200

    Added simple java program

--- src/project1.java
+++ src/project1.java
@@ -1,7 +1,23 @@
 
-public class project1 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-	   public static void main(String[] args) {
-	        System.out.print("test");
+
+public class project1 {
+	
+	   public static void main(String[] args) throws IOException 
+	   {
+	        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+	        System.out.print("Enter Integer:");
+	        try
+	        {
+	            int number = Integer.parseInt(reader.readLine());
+	            System.out.println("You entered: " + number);
+	        }
+	        catch(NumberFormatException error)
+	        {
+	            System.err.println("Invalid Format!");
+	        }
 	    }
 }
